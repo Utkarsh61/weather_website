7d5719feb921fd74a8b6ee1e4c5da6d2065e7e90
Utkarsh Tripathi <98511619+Utkarsh61@users.noreply.github.com>
2023-08-17 13:51:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Temp from "./components/weather/temp";
+
+function App() {
+  return (
+    <>
+      <Temp/>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/weather/temp.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import "./style.css";
+import Weathercard from './weathercard';
+
+
+const Temp = () => {
+
+       const [searchValue,setSearchValue]=useState("Pune");
+
+       const [tempInfo,setTempInfo]=useState({});
+
+       const getWeatherInfo=async()=>{
+          try{
+            let url= `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=04ff1f80662cc86797a0c1254351b641`;
+             
+              const res=await fetch(url);
+              const data= await res.json();
+
+              const {temp,humidity,pressure}=data.main;
+              const {main : weathermood }=data.weather[0];
+              const {name}=data;
+             const {speed}=data.wind;
+             const {country,sunset}=data.sys;
+
+             const myNewWeatherInfo={
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset
+
+             };
+
+             setTempInfo(myNewWeatherInfo);
+
+          } catch(error){
+            console.log(error)
+          }
+
+       }
+
+       useEffect(()=>{
+        
+        getWeatherInfo();
+          
+       },[]);
+
+  return (
+    <>
+       <div className='wrap'>
+          <div className='search'>
+             <input type='search'
+             placeholder='search...' 
+             autoFocus
+             id="search"
+             className='searchTerm'
+             value={searchValue}
+             onChange={(e)=>setSearchValue(e.target.value)}
+             />
+               
+              <button className='searchButton' 
+                  type="button" 
+                  onClick={getWeatherInfo}>
+                  search
+              </button>
+
+          </div>
+
+        </div>
+       {/* {our temp card} */}
+       <Weathercard tempInfo={tempInfo}/>
+       
+       
+    </>
+  )
+}
+
+export default Temp
